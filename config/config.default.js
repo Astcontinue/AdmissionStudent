3a8e6aa03c6a89eb045d67bb20602c1c6be9c10c
DS裕又 <1462451723@qq.com>
2024-01-15 04:29:23 +0800

    modified:   config/config.default.js 代码更改

--- config/config.default.js
+++ config/config.default.js
@@ -17,7 +17,7 @@ module.exports = appInfo => {
 
   return {
     appId,
-    appTitle: '1张表CRUD',
+    appTitle: '招生系统',
     appLogo: `${appId}/public/img/logo.png`,
     appType: 'single',
     appDirectoryLink: '/',
